f8cd4c504a9dff4847ce7aebe650afe417e82d81
Tahazzat-dev <mdtahazzatali820@gmail.com>
2025-10-04 11:11:30 +0600

    added header responsiveness

---
+++ src/sharedComponets/header/PublicSidebar.tsx
@@ -0,0 +1,126 @@
+import { Menu, Search, X } from "lucide-react"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+
+export function MenuTabSwitcher() {
+    return (
+        <div className="flex w-full  flex-col gap-6">
+            <Tabs defaultValue="main-menu">
+                <TabsList className="w-full !p-0 cd_rounded-sm overflow-hidden">
+                    <TabsTrigger className="w-full bg-slate-100 text-black data-[state=active]:bg-[var(--primary)] data-[state=active]:text-white py-2" value="main-menu">Main Menu</TabsTrigger>
+                    <TabsTrigger className="w-full bg-slate-100 text-black data-[state=active]:bg-[var(--primary)] data-[state=active]:text-white py-2" value="category">Category</TabsTrigger>
+                </TabsList>
+                <TabsContent value="main-menu">
+                    <ul className="flex flex-col">
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/" className="hover:text-primary transition-colors">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/shop" className="hover:text-primary transition-colors">
+                                Shop
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/vendor" className="hover:text-primary transition-colors">
+                                Vendor
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/blogs" className="hover:text-primary transition-colors">
+                                Blogs
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/elements" className="hover:text-primary transition-colors">
+                                Elements
+                            </Link>
+                        </li>
+                    </ul>
+
+                </TabsContent>
+                <TabsContent value="category">
+                    <ul className="flex flex-col">
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/fashion" className="hover:text-primary transition-colors">
+                                Fashion
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/home-and-garden" className="hover:text-primary transition-colors">
+                                Home & Garden
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/electronics" className="hover:text-primary transition-colors">
+                                Electronics
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/furniture" className="hover:text-primary transition-colors">
+                                Furniture
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/health-and-beauty" className="hover:text-primary transition-colors">
+                                Health & Beauty
+                            </Link>
+                        </li>
+                        <li className="border-b cd_border py-2.5 px-2">
+                            <Link href="/toy-and-games" className="hover:text-primary transition-colors">
+                                Toy & Games
+                            </Link>
+                        </li>
+                    </ul>
+                </TabsContent>
+            </Tabs>
+        </div>
+    )
+}
+
+
+
+import * as React from "react"
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import Link from "next/link"
+export default function PublicSidebar() {
+    return (
+        <Drawer direction="left" >
+            <DrawerTrigger asChild>
+                <Menu className='w-5 h-5 lg:hidden' />
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="w-full h-full bg-red-40 p-5">
+                    <div className="w-full flex items-center gap-5 mb-4">
+                        <div className="grow flex border border-slate-300 cd_rounded-sm">
+                            <input type="text" className='grow cd_fs-sm px-3 focus:border-0 py-1.5 focus:outline-0' placeholder='Search in...' />
+                            <button className='mr-4 '>
+                                <Search className='w-5 h-5 duration-300  hover:scale-105' />
+                            </button>
+                        </div>
+
+                        <div className="hidden sm:block">
+                            <DrawerClose>
+                                <X className="w-5 md:w-6 md:h-6 h-5" />
+                            </DrawerClose>
+                        </div>
+                    </div>
+                    <div className="w-full h-full">
+                        <MenuTabSwitcher />
+                    </div>
+                </div>
+            </DrawerContent>
+        </Drawer>
+    )
+}
+
--- src/sharedComponets/header/SiteLogo.tsx
+++ src/sharedComponets/header/SiteLogo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function SiteLogo() {
     return (
         <div className='-mb-2.5'>
-            <Image className='w-full max-w-[190px] h-auto' width={190} height={42} src="/images/shared/CellsDeal.png" alt="Site logo" />
+            <Image className='w-full min-w-[150px] lg:min-w-[180px] max-w-[190px] h-auto' width={190} height={42} src="/images/shared/CellsDeal.png" alt="Site logo" />
         </div>
     )
 }
