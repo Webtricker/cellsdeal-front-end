0d00d7377377d336210c9361c10862371459035c
dev-ahad-ali <ahadali.dev@gmail.com>
2025-10-06 10:41:31 +0600

    Fix: public home page spelling

--- src/app/(public pages)/(home)/page.tsx
+++ src/app/(public pages)/(home)/page.tsx
@@ -3,7 +3,7 @@ import JustForYou from './sections/JustForYou';
 import MultipleInterests from './sections/MultipleInterests';
 import SalesProducts from './sections/SalesProducts';
 
-export default function PubliceHomePage() {
+export default function PublicHomePage() {
   return (
     <>
       <MultipleInterests />
