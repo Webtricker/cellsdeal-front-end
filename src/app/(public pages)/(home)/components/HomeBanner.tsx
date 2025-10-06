b4aa6c70a80a1a52a6ec3e8f6864f59967e5fc3a
Tahazzat-dev <mdtahazzatali820@gmail.com>
2025-10-06 10:35:41 +0600

    banner added

--- src/app/(public pages)/(home)/components/HomeBanner.tsx
+++ src/app/(public pages)/(home)/components/HomeBanner.tsx
@@ -4,38 +4,43 @@ import React from 'react'
 import BannerSlider from './BannerSlider'
 import { SwiperSlide } from 'swiper/react';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const Banner = () => {
     return <>
-        <div className="w-[65%] relative cd_rounded overflow-hidden">
-            <BannerSlider className='w-full'>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'>
-                    <div className='flex'>
-                        <div className="grow"></div>
-                        <div className="content">
-                            <h2>NEW COLLECTION</h2>
-                            <h3 className='cd_fs-3xl'>Electronics Sale</h3>
-                            <h4>Starting at $299.99</h4>
-                            <Button variant="outline">
+        <div className="w-full md:w-[65%] relative cd_rounded overflow-hidden">
+            <BannerSlider className='w-full h-[60vh] max-h-[400px] lg:max-h-[450px] min-h-[350px] lg:min-h-[400px]'>
+                <SwiperSlide className='banner-gradient h-full overflow-hidden border w-full p-5 lg:p-10 xl:p-14'>
+                    <div className='flex h-full gap-[4%] lg:gap-[7%] w-full'>
+                        <div className="w-[48%] lg:w-[45%] p-2 lg:p-5 flex items-center justify-center">
+                            <Image className='w-full lg:w-[90%] object-cover' src="/images/home/i-phone-image.png" width={300} height={400} alt="" />
+                        </div>
+                        <div className="w-[48%] h-full flex flex-col items-start justify-center text-white bg-transparent">
+                            <h2 className='font-semibold cd_fs-2xl mb-1'>NEW COLLECTION</h2>
+                            <h3 className='cd_fs-xl'>Electronics Sale</h3>
+                            <h4 className='cd_fs-xl mb-4'>Starting at $299.99</h4>
+                            <Button className="bg-transparent" variant="outline" >
                                 Shop Now
                             </Button>
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div></SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 4</SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 5</SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 6</SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 7</SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 8</SwiperSlide>
-                <SwiperSlide className='banner-gradient p-4 md:p-5 min-h-[400px]'><div className='bg-white'>
-                </div> 9</SwiperSlide>
+                <SwiperSlide className='banner-gradient overflow-hidden h-full  w-full p-5 lg:p-10 xl:p-14'>
+                    <div className='flex h-full gap-[4%] lg:gap-[7%] items-center w-full'>
+
+                        <div className="w-[48%] h-full flex flex-col items-end justify-center text-white bg-transparent">
+                            <h2 className='font-semibold cd_fs-2xl mb-1'>Mega Sale</h2>
+                            <h3 className='cd_fs-xl'>Special Offer</h3>
+                            <h4 className='cd_fs-xl text-right mb-4'>Starting at $299.99</h4>
+                            <Button className="bg-transparent" variant="outline" >
+                                Shop Now
+                            </Button>
+                        </div>
+                        <div className=" w-[48%] lg:w-[45%] p-2 lg:p-5 flex items-center justify-center">
+                            <Image src="/images/home/mega-sell-1.png" width={300} height={400} alt="" />
+                        </div>
+                    </div>
+                </SwiperSlide>
             </BannerSlider>
         </div>
     </>
@@ -44,7 +49,8 @@ const Banner = () => {
 
 const HotDeals = () => {
     return <>
-        <div className="grow bg-red-300 cd_rounded overflow-hidden">
+        <div className="w-full md:w-[unset] min-h-[200px] md:grow bg-red-300 cd_rounded overflow-hidden p-5 lg:p-10 xl:p-14">
+                <h2>Hot Deals of The Day</h2>
 
         </div>
     </>
@@ -54,7 +60,7 @@ const HotDeals = () => {
 export default function HomeBanner() {
     return (
         <section className='w-full mt-5'>
-            <Container className='flex gap-5 justify-between overflow-hidden'>
+            <Container className='flex flex-col md:flex-row gap-5 justify-between overflow-hidden'>
                 <Banner />
                 <HotDeals />
             </Container>
