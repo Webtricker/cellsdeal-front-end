c9cb5eedf83d1083f740d58591f6733bc1165a58
Tahazzat-dev <mdtahazzatali820@gmail.com>
2025-10-06 11:53:41 +0600

    fixed banner

--- src/app/(public pages)/(home)/components/CountDown.tsx
+++ src/app/(public pages)/(home)/components/CountDown.tsx
@@ -1,44 +1,63 @@
 import { useEffect, useState } from "react";
 
 type CountdownProps = {
-  targetDate: string; // e.g. "2025-12-31T23:59:59"
+    targetDate?: string; // e.g. "2025-12-31T23:59:59"
 };
 
-export default function Countdown({ targetDate }: CountdownProps) {
-  const calculateTimeLeft = () => {
-    const difference = +new Date(targetDate) - +new Date();
-    let timeLeft = {
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
+export default function Countdown({ }: CountdownProps) {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 6);
+
+    const calculateTimeLeft = () => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        };
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
     };
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-    return timeLeft;
-  };
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+        return () => clearInterval(timer);
+    }, [targetDate]);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    return (
+        <div className="flex items-center justify-center gap-2 flex-wrap mt-5 lg:mt-10">
+            <div className="bg-white p-2 cd_rounded flex flex-col items-center min-w-10">
+                <span className="cd_fs-lg font-bold leading-[100%]">{timeLeft.days}</span>
+                <span className="cd_fs-xs">Days</span>
+            </div>
+            <div className="bg-white p-2 cd_rounded flex flex-col items-center min-w-10">
+                <span className="cd_fs-lg font-bold leading-[100%]">{timeLeft.hours}</span>
+                <span className="cd_fs-xs">Hour</span>
+            </div>
+            <div className="bg-white p-2 cd_rounded flex flex-col items-center min-w-10">
+                <span className="cd_fs-lg font-bold leading-[100%]">{timeLeft.minutes}</span>
+                <span className="cd_fs-xs">Min</span>
+            </div>
 
-    return () => clearInterval(timer);
-  }, [targetDate]);
-
-  return (
-    <div className="text-center text-2xl font-bold">
-      {timeLeft.days}d : {timeLeft.hours}h : {timeLeft.minutes}m : {timeLeft.seconds}s
-    </div>
-  );
+            <div className="bg-white p-2 cd_rounded flex flex-col items-center min-w-10">
+                <span className="cd_fs-lg font-bold leading-[100%]">{timeLeft.seconds}</span>
+                <span className="cd_fs-xs">Sec</span>
+            </div>
+        </div>
+    );
 }
--- src/app/(public pages)/(home)/components/HomeBanner.tsx
+++ src/app/(public pages)/(home)/components/HomeBanner.tsx
@@ -5,6 +5,7 @@ import BannerSlider from './BannerSlider'
 import { SwiperSlide } from 'swiper/react';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Countdown from './CountDown';
 
 const Banner = () => {
     return <>
@@ -16,9 +17,9 @@ const Banner = () => {
                             <Image className='w-full lg:w-[90%] object-cover' src="/images/home/i-phone-image.png" width={300} height={400} alt="" />
                         </div>
                         <div className="w-[48%] h-full flex flex-col items-start justify-center text-white bg-transparent">
-                            <h2 className='font-semibold cd_fs-2xl mb-1'>NEW COLLECTION</h2>
-                            <h3 className='cd_fs-xl'>Electronics Sale</h3>
-                            <h4 className='cd_fs-xl mb-4'>Starting at $299.99</h4>
+                            <h2 className='font-semibold text-lg md:!text-2xl xl:!text-3xl 2xl:!text-4xl mb-1'>New Collection</h2>
+                            <h3 className='md:!text-lg lg:!text-xl xl:!text-2xl'>Electronics Sale</h3>
+                            <h4 className='md:!text-lg lg:!text-xl xl:!text-2xl mb-4'>Starting at $299.99</h4>
                             <Button className="bg-transparent" variant="outline" >
                                 Shop Now
                             </Button>
@@ -29,9 +30,9 @@ const Banner = () => {
                     <div className='flex h-full gap-[4%] lg:gap-[7%] items-center w-full'>
 
                         <div className="w-[48%] h-full flex flex-col items-end justify-center text-white bg-transparent">
-                            <h2 className='font-semibold cd_fs-2xl mb-1'>Mega Sale</h2>
-                            <h3 className='cd_fs-xl'>Special Offer</h3>
-                            <h4 className='cd_fs-xl text-right mb-4'>Starting at $299.99</h4>
+                            <h2 className='font-semibold text-lg md:!text-2xl xl:!text-3xl 2xl:!text-4xl mb-1'>Mega Sale</h2>
+                            <h3 className='md:!text-lg lg:!text-xl xl:!text-2xl'>Special Offer</h3>
+                            <h4 className='md:!text-lg lg:!text-xl xl:!text-2xl text-right mb-4'>Starting at $299.99</h4>
                             <Button className="bg-transparent" variant="outline" >
                                 Shop Now
                             </Button>
@@ -49,9 +50,15 @@ const Banner = () => {
 
 const HotDeals = () => {
     return <>
-        <div className="w-full md:w-[unset] min-h-[200px] md:grow bg-red-300 cd_rounded overflow-hidden p-5 lg:p-10 xl:p-14">
-                <h2>Hot Deals of The Day</h2>
-
+        <div className="w-full md:w-[unset] min-h-[200px] z-0 relative items-center justify-between md:grow flex flex-col bg-amber-500 cd_rounded overflow-hidden p-5 lg:p-10 xl:p-14">
+            <h2 className='text-center text-white'>Hot Deals of The Day</h2>
+            <Image src="/images/home/air-pod.png" className='hidden lg:block absolute top-[50%] left-0 translate-y-[-50%] -z-10 w-full h-auto' width={300} height={200} alt="" />
+            <div className="flex flex-col gap-4 items-center">
+                <Countdown />
+                <Button className="bg-transparent text-white" variant="outline" >
+                    Buy Now
+                </Button>
+            </div>
         </div>
     </>
 }
